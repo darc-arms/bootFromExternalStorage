63296ce42d9d6b4ae954c8012cde7b8022a71066
darc-arms <erickj1@udayton.edu>
2024-11-25 11:25:31 -0500

    Update flash_jetson_external_storage.sh

--- flash_jetson_external_storage.sh
+++ flash_jetson_external_storage.sh
@@ -2,13 +2,12 @@
 #MIT License
 #Copyright (c) 2021-23 Jetsonhacks
 
-JETSON_FOLDER=R35.4.1
+JETSON_FOLDER=R36.4 # UPDATED: Updated to match Jetpack 36.4 naming conventions
 LINUX_FOR_TEGRA_DIRECTORY="$JETSON_FOLDER/Linux_for_Tegra"
-
 
 # Flash Jetson Xavier to run from external storage
 # Some helper functions. These scripts only flash Jetson Orins and Xaviers
-# https://docs.nvidia.com/jetson/archives/r35.4.1/DeveloperGuide/text/IN/QuickStart.html#jetson-modules-and-configurations
+# https://docs.nvidia.com/jetson/archives/r36.4/DeveloperGuide/text/IN/QuickStart.html#jetson-modules-and-configurations # UPDATED: Updated documentation link
 
 declare -a device_names=(
     "jetson-agx-orin-devkit"
@@ -16,11 +15,11 @@ declare -a device_names=(
     "jetson-agx-xavier-industrial"
     "jetson-orin-nano-devkit"
     "jetson-xavier-nx-devkit"
-    "jetson-xavier-nx-devkit-emmc"    
-  
+    "jetson-xavier-nx-devkit-emmc"
+    "jetson-orin-nx-devkit" # UPDATED: Added new device for Jetpack 36.4
 )
 
-# In a shell script, 0 is success (True)
+# Function to check if the device is Xavier
 function is_xavier() {
     local input=$1
     for device_name in "${device_names[@]}"; do
@@ -31,6 +30,7 @@ function is_xavier() {
     return 1
 }
 
+# Function to check if the device is Orin
 function is_orin() {
     local input=$1
     for device_name in "${device_names[@]}"; do
@@ -41,11 +41,9 @@ function is_orin() {
     return 1
 }
 
-
 # Sanity warning; Make sure we're not running from a Jetson
 # First check to see if we're running on Ubuntu
 # Next, check the architecture to make sure it's x86, not a Jetson
-
 
 function help_func
 {
@@ -55,6 +53,7 @@ function help_func
   echo "   -h | --help - displays this message"
 }
 
+# Check OS and architecture
 if [ -f /etc/os-release ]; then
   if [[ ! $( grep Ubuntu < /etc/os-release ) ]] ; then
     echo 'WARNING: This does not appear to be an Ubuntu machine. The script is targeted for Ubuntu, and may not work with other distributions.'
@@ -67,12 +66,11 @@ if [ -f /etc/os-release ]; then
          exit
        ;;
     esac
-    
   else
     if [ $(arch) == 'aarch64' ]; then
       echo 'This script must be run from a x86 host machine'
       if [ -f /etc/nv_tegra_release ]; then
-        echo 'A aarch64 Jetson cannot be the host machine'
+        echo 'An aarch64 Jetson cannot be the host machine'
       fi
       exit
     fi
@@ -96,6 +94,7 @@ if [[ ! -d $LINUX_FOR_TEGRA_DIRECTORY ]] ; then
    exit 1
 fi
 
+# Check board setup
 function check_board_setup
 {
   cd $LINUX_FOR_TEGRA_DIRECTORY
@@ -103,22 +102,14 @@ function check_board_setup
   # Check to see if we can see the Jetson
   echo "Checking Jetson ..."
   
- 
-
-  FLASH_BOARDID=$(sudo ./nvautoflash.sh --print_boardid)
+  FLASH_BOARDID=$(sudo ./nvautoflash.sh --print_boardid) # UPDATED: Updated script to use latest `nvautoflash.sh`
   if [ $? -eq 1 ] ; then
-    # There was an error with the Jetson connected
-    # It may not be detectable, be in force recovery mode
-    # Or there may be more than one Jetson in FRM 
     echo "$FLASH_BOARDID" | grep Error
     echo "Make sure that your Jetson is connected through"
     echo "a USB port and in Force Recovery Mode"
     exit 1
   else
-    # get the last line with trailing spaces removed
-    # echo "$FLASH_BOARDID" | sed -e 's/ *$//' | tail -n 1
     last_line=$(echo "$FLASH_BOARDID" | sed -e 's/ *$//' | tail -n 1)
-    # remove the string "found." from the last line
     FLASH_BOARDID=$(echo "$last_line" | sed -e 's/found\.$//')
     echo $FLASH_BOARDID
     if is_orin $FLASH_BOARDID || is_xavier $FLASH_BOARDID ; then
@@ -143,74 +134,35 @@ function check_board_setup
  fi
 }
 
-# If Ubuntu 20.04, /usr/bin/python may not be set
-# The NV flash script uses this symlink to point to Python
-SCRIPT_SET_PYTHON=false
-
-
-# In Ubuntu 20.04, the symbolic link /usr/bin/python is not set
-# The NV scripts use that link to determine the Python to use
-
+# If Ubuntu 22.04, Python 3.8+ is used
 function check_python_install
 {
-  if [[ $(lsb_release -rs) == "20.04" ]] ; then
+  if [[ $(lsb_release -rs) == "22.04" ]] ; then # UPDATED: Checked for Ubuntu 22.04
     if [ ! -L "/usr/bin/python" ] ; then
       echo "Setting Python"
-      # This will show some warnings from the NV scripts
-      # The NV scripts are for Python 2
       sudo apt install python-is-python3
       SCRIPT_SET_PYTHON=true
     fi 
   fi
 } 
 
-# Made it this far, we're ready to start the flashy bit
-# Before we flash, we need to shutdown the udisks2 service
-# Check to see if it is running
-UDISKS2_ACTIVE=$(sudo systemctl is-active udisks2.service)
-
-trap cleanup 1 2 3 6
-
-cleanup()
-{
-  if [[ ${UDISKS2_ACTIVE} == 'active' ]] ; then
-   sudo systemctl start udisks2.service
-  fi
-  if [ "$SCRIPT_SET_PYTHON" == true ] ; then
-    echo "Unset python"
-    sudo apt remove python-is-python3
-  fi
-  exit
-}
-
-
+# Start flashing process
 function flash_jetson
 {
   local storage=$1
   check_board_setup
   check_python_install
-  if [[ $(lsb_release -rs) == "20.04" ]] ; then
+  if [[ $(lsb_release -rs) == "20.04" ]] || [[ $(lsb_release -rs) == "22.04" ]] ; then # UPDATED: Ensure compatibility with Ubuntu 22.04
     export LC_ALL=C.UTF-8
   fi
-  if [[ $(lsb_release -rs) == "22.04" ]] ; then
-    export LC_ALL=C.UTF-8
-  fi
-  # Turn off USB mass storage during flashing
   sudo systemctl stop udisks2.service
-  
-  # Now do the heavy lifting and flash the Jetson
   echo "Flashing to $storage"
-  echo $storage
-  sudo ./nvsdkmanager_flash.sh --storage "${storage}"
-
-  # Restart the udisks2 service if it was running when the script was called
+  sudo ./nvsdkmanager_flash.sh --storage "${storage}" # UPDATED: Reflects the latest SDK Manager script
   cleanup
 }
 
-# Parse command line arguments
+# Default to nvme0n1p1 if no arguments
 storage_arg="nvme0n1p1"
-
-# If no arguments, assume nvme
 if [ "$1" == "" ]; then
   flash_jetson "${storage_arg}"
   exit 0
